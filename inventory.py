a28884e7ea3ad91013c7f613598a7e21126132e7
shazinnausar-spec <shazinnausar@gmail.com>
2026-06-17 19:08:08 +0530

    save prducts

--- inventory.py
+++ inventory.py
@@ -8,3 +8,7 @@ def load_products():    #creating a function called Load_products
         with open(FILENAME, "r") as file:
             return json.load(file)
     return []
+# save products 
+def save_products(products):   #creating a function called save_products
+    with open(FILENAME, "w") as file:
+        json.dump(products, file, indent=4)
