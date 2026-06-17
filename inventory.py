257724edfcba9a0716265f55da5390995f8322f4
shazinnausar-spec <shazinnausar@gmail.com>
2026-06-17 19:00:59 +0530

    Load products

--- inventory.py
+++ inventory.py
@@ -3,3 +3,8 @@ import os
 
 FILENAME = "products.json"
 #load porducts
+def load_products():    #creating a function called Load_products
+    if os.path.exists(FILENAME):
+        with open(FILENAME, "r") as file:
+            return json.load(file)
+    return []
