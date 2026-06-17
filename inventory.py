40c9b372d932b3ddc83d04572168f10e29fdb7e5
shazinnausar-spec <shazinnausar@gmail.com>
2026-06-17 19:15:30 +0530

    add product

--- inventory.py
+++ inventory.py
@@ -12,3 +12,20 @@ def load_products():    #creating a function called Load_products
 def save_products(products):   #creating a function called save_products
     with open(FILENAME, "w") as file:
         json.dump(products, file, indent=4)
+# add product
+def add_product(products):
+    porduct_id = input("enter product id: ")
+    name = input("enter product name: ")
+    quantity = int(input("enter product quantity: ")) 
+    price = float(input("enter product price: "))
+
+    product = {
+        "id": product_id,
+        "name": name,
+        "quantity": quantity,
+        "price":price
+    }
+    products.append(product)
+    save_products(products)
+    print("\nProduct Added Successfully!\n")
+    
